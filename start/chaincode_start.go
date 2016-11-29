c25e98e67574f8febc0346ff10c921aa921ecff6
Vinod Kannan <vinodkannan4141@gmail.com>
2016-11-29 12:10:55 +0400

    compiled my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -42,7 +42,10 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
-
+	err := stub.PutState("hello_world", []byte(args[0]))
+	if err != nil {
+		return nil, err
+	}
 	return nil, nil
 }
 
@@ -53,6 +56,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else if function == "write" {
+		return t.write(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 
@@ -68,7 +73,53 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		fmt.Println("hi there " + function)						//error
 		return nil, nil;
 	}
+
+	if function == "read" {
+		return t.read(stub, args)
+	}
 	fmt.Println("query did not find func: " + function)						//error
 
 	return nil, errors.New("Received unknown function query: " + function)
+}
+
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error)  {
+	var name, value string
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments")
+	}
+
+	if (len(args) == 4){
+		fmt.Println("My Code Running");
+		err = stub.PutState(args[0], []byte(args[1]))
+		err = stub.PutState(args[2], []byte(args[3]))
+	} else {
+		name = args[0]
+		value = args[1]
+		err = stub.PutState(name, []byte(value))
+	}
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func ( t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error)  {
+	var name, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments")
+	}
+
+	name = args[0]
+	valAsbytes, err := stub.GetState(name)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
 }
